9f92c30f1da751ea7c1ed651444fda056df82ebd
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2024-04-24 19:47:46 +0200

    fiirebase test on simple auth

--- src/app/account/components/login/login.component.ts
+++ src/app/account/components/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private auth: AngularFireAuth) {}
+
+  loginWithEmail() {
+    this.auth.signInWithEmailAndPassword(this.email, this.password)
+      .then(() => {
+        console.log('well done')
+      })
+      .catch(error => {
+        this.errorMessage = error.message;
+      });
+  }
 }
