32e089ca205364b6ce32a8c98a4bafe2ad15db7b
JustynaGargula <s221391@student.uek.krakow.pl>
2024-04-29 20:04:46 +0200

    account data in firebase

--- src/app/account/components/account-settings/account-settings.component.ts
+++ src/app/account/components/account-settings/account-settings.component.ts
@@ -14,11 +14,40 @@ import { AccountService } from '../../services/account.service';
 export class AccountSettingsComponent {
 
   public user: User;
+
   constructor(public service: AccountService){
     this.user = service.currentUser
   }
 
+  ngOnInit(){
+    console.log("user ID", this.service.userId)
+    try{
+      this.service.getUserData().then((data) => {this.user = data; console.log("user data", data)})
+      // console.log("user data", this.user)
+    }
+    catch(error){
+      console.error("Please log in to continue");
+    }
+
+  }
+
   onSubmit(form: NgForm) {
-    console.log("Submitted form", form.value())
+    if(form.valid){
+      this.service.saveUserData(this.user)
+      console.log("Saved data")
+    }
+    else{
+      console.log("Not valid form")
+    }
+
+  }
+
+  public getFullName(){
+    if(this.user.surname==""){
+      return "John Doe"
+    }
+    else{
+      return this.user.name+" "+this.user.surname
+    }
   }
 }
--- src/app/account/services/account.service.ts
+++ src/app/account/services/account.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import {Router} from "@angular/router";
 import firebase from 'firebase/compat/app';
 import {User} from "../models/user";
+import { Firestore, collection, getDocs, query } from '@angular/fire/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +12,18 @@ import {User} from "../models/user";
 export class AccountService {
 
   currentUser: User = {name: "", surname: "", email: "", phone: ""};
-
+  userId: any;
+  private firestore: Firestore = inject(Firestore)
+  
   constructor(private auth: AngularFireAuth, private router: Router) {
   }
 
   loginWithEmail(email: string, password: string) {
     return this.auth.signInWithEmailAndPassword(email, password)
-      .then(() => {
+      .then((result) => {
         console.log('Logowanie udane');
         this.router.navigate(['/']);
+        this.userId = result.user?.uid
       })
       .catch(error => {
         return Promise.reject(error.message);
@@ -26,9 +31,10 @@ export class AccountService {
   }
   loginWithGoogle() {
     return this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-      .then(() => {
+      .then((result) => {
         console.log('Logowanie przez Google udane');
         this.router.navigate(['/']);
+        this.userId = result.user?.uid
       })
       .catch(error => {
         return Promise.reject(error.message);
@@ -37,9 +43,10 @@ export class AccountService {
 
   loginWithFacebook() {
     return this.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
-      .then(() => {
+      .then((result) => {
         console.log('Logowanie przez Facebooka udane');
         this.router.navigate(['/']);
+        this.userId = result.user?.uid
       })
       .catch(error => {
         return Promise.reject(error.message);
@@ -50,11 +57,48 @@ export class AccountService {
     this.currentUser = user;
     return this.auth.createUserWithEmailAndPassword(user.email, password)
       .then((result) => {
-        console.log('Rejestracja udana');
         this.router.navigate(['/login']);
+
+        this.userId = result.user?.uid
+        this.saveUserData(this.currentUser)
       })
       .catch(error => {
         return Promise.reject(error.message);
       });
     }
+
+  async saveUserData(data: Object) {
+    try {
+      await setDoc(doc(this.firestore, 'user_data', this.userId), data).then(() => { console.log("wysłano dokument") })
+    }
+    catch (error) {
+      console.error("Wystąpił błąd:", error)
+    }
+  }
+
+  async getUserData() {
+    try{
+      let user_data = (await getDoc(doc(this.firestore, "user_data", this.userId)));
+      if (user_data.exists()) {
+        this.currentUser.name = user_data.data()['name'];
+        this.currentUser.surname = user_data.data()['surname']
+        this.currentUser.email = user_data.data()['email'];
+        this.currentUser.phone = user_data.data()['phone']
+
+        // console.log("current user", this.currentUser)
+        return this.currentUser
+      }
+
+      else {
+        console.log("No current user!")
+        return this.currentUser
+      }      
+    }
+    catch(error){
+      console.error("Please log in to continue", error);
+      return this.currentUser
+    }
+
+  }
+
 }
