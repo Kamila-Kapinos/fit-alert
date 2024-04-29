583f0ec11645ab8b69e82a5ddc3b3a1a6131a66b
Bartek <s222936@student.uek.krakow.pl>
2024-04-30 00:08:15 +0200

    connected articles service to db and article component to article service

--- src/app/articles/article/article.component.ts
+++ src/app/articles/article/article.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ArticlesService } from './services/articles.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
+  articles:any;
+  article:any;
+  src:string='';
+  alt:string='';
+  footer:string='';
+  constructor (public articleService:ArticlesService){
 
+  }
+
+  ngOnInit():void{
+    this.articleService.getArticles()
+      .then((ar)=>{
+        this.articles = ar;console.log("success getting articles!");
+        this.article = this.articles[0]
+        console.log(this.article)
+        this.src = this.article['article-img-url'];
+        this.alt = this.article['article-img-alt'];
+        this.footer = this.article['article-footer'];
+      })
+      .catch(()=>{console.log("error getting articles")})
+  }
 }
--- src/app/articles/article/services/articles.service.ts
+++ src/app/articles/article/services/articles.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collection,doc, getDoc, getDocs, query } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
 
-  constructor() { }
+  private firestore:Firestore = inject(Firestore)
+  constructor() { 
+    this.getArticles();
+  }
+  async getArticles(amount:number=4){
+    let articles:any = (await(getDocs(query(collection(this.firestore,'articles')))))
+      .docs.map((articles)=>articles.data());
+    console.log(articles);
+    return articles;
+  }
 }
