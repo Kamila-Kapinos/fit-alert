eee1e157f2a781b2335757e5afcefeacc4588ed9
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2024-05-01 12:36:24 +0200

    adding articles from fire store to home page

--- src/app/articles/article/services/articles.service.ts
+++ src/app/articles/article/services/articles.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, getDocs, query } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
+  constructor(private firestore: Firestore) { }
 
-  constructor() { }
+  async getArticles() {
+    const querySnapshot = await getDocs(collection(this.firestore, 'articles'));
+    return querySnapshot.docs.map(doc => doc.data());
+  }
 }
