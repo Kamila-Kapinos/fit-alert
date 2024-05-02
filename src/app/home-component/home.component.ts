9fd7d6e5ea04d5afa4a327f7e49c2b509a650395
JustynaGargula <s221391@student.uek.krakow.pl>
2024-05-02 18:36:47 +0200

    added vibration on notification

--- src/app/home-component/home.component.ts
+++ src/app/home-component/home.component.ts
@@ -4,6 +4,7 @@ import {NgForOf} from "@angular/common";
 import firebase from "firebase/compat";
 import QuerySnapshot = firebase.firestore.QuerySnapshot;
 import {RouterLink} from "@angular/router";
+import { NotificationsService } from '../services/notifications.service';
 
 @Component({
   selector: 'app-home',
@@ -22,7 +23,7 @@ export class HomeComponent implements OnInit {
   challenges = ['Eat two carrots', 'Prepare a veggie meal', 'Do 10 push-ups'];
   trendingArticles: any[] = [];
 
-  constructor(private articlesService: ArticlesService) { }
+  constructor(private articlesService: ArticlesService, private notificationsService: NotificationsService) { }
 
   ngOnInit(): void {
     this.yourChallenge = this.getRandomChallenge();
@@ -41,6 +42,10 @@ export class HomeComponent implements OnInit {
   getRandomChallenge(): string {
     const randomIndex = Math.floor(Math.random() * this.challenges.length);
     return this.challenges[randomIndex];
+  }
+
+  challengeCompleted(){
+    this.notificationsService.sendNotification("Challenge completed", "Congrats on completing challenge! Keep up good work!")
   }
 }
 
