25d1f2cf321787f4bb106bb809f6331125df4847
JustynaGargula <s221391@student.uek.krakow.pl>
2024-05-03 23:28:21 +0200

    Prepared map component for further development

---
+++ src/app/activities/services/map.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, doc, getDocs, query, setDoc } from '@angular/fire/firestore';
+import { randomUUID } from 'crypto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+  private firestore: Firestore = inject(Firestore);
+  private userId: any;
+  
+  constructor() { }
+
+  async saveActivity(data: Object) {
+    try {
+      await setDoc(
+        doc(
+          this.firestore,
+          'users/' + this.userId + '/map',
+        ),
+        data,
+      ).then(() => {
+        console.log('wysłano dokument');
+      });
+    } catch (error) {
+      console.error('Wystąpił błąd:', error);
+    }
+  }
+
+  async getActivitiesLocations() {
+    return (
+      await getDocs(
+        query(collection(this.firestore, 'users/' + this.userId + '/map')),
+      )
+    ).docs.map((activities) => activities.data());
+  }
+
+}
