41b487bf3ecb483fe730323c806391d981f8b161
JustynaGargula <s221391@student.uek.krakow.pl>
2024-05-04 14:50:10 +0200

    Prepared activities buttons on map

--- src/app/activities/components/map/map.component.ts
+++ src/app/activities/components/map/map.component.ts
@@ -14,7 +14,10 @@ import { DatePipe } from '@angular/common';
 export class MapComponent implements OnInit {
   private map!: Map;
 
-  constructor(private mapService: MapService, private datePipe: DatePipe) { }
+  constructor(
+    private mapService: MapService,
+    private datePipe: DatePipe,
+  ) {}
 
   ngOnInit(): void {
     const customIcon = L.icon({
@@ -30,7 +33,7 @@ export class MapComponent implements OnInit {
     L.Marker.prototype.setIcon(customIcon);
 
     const mapOptions = {
-      center:  new LatLng(50.061822, 19.93701),
+      center: new LatLng(50.061822, 19.93701),
       zoom: 10,
     };
     this.map = L.map('map', mapOptions);
@@ -39,8 +42,8 @@ export class MapComponent implements OnInit {
       'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
         attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          +'<a href="https://www.flaticon.com/free-icons/venue" title="venue icons"> | Here icon by IYIKON - Flaticon</a>',
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' +
+          '<a href="https://www.flaticon.com/free-icons/venue" title="venue icons"> | Here icon by IYIKON - Flaticon</a>',
       },
     );
     this.map.addLayer(layer);
@@ -57,11 +60,12 @@ export class MapComponent implements OnInit {
   onLocationFound(e: any) {
     const radius = e.accuracy;
 
-    L.marker(e.latlng, {icon: L.icon({
-      iconUrl: "assets/media/current-location.png",
-      iconSize: [41, 41],
+    L.marker(e.latlng, {
+      icon: L.icon({
+        iconUrl: 'assets/media/current-location.png',
+        iconSize: [41, 41],
+      }),
     })
-      })
       .addTo(this.map)
       .bindPopup('You are here')
       .openPopup();
@@ -72,16 +76,44 @@ export class MapComponent implements OnInit {
   }
 
   onMapClick(e: any) {
-    const info = '<p>Coordinates: ' + this.formatCoordinatesToDispplay(e.latlng) + '</p>';
-    const button = '<button class="btn btn-dark" id="addActivityBtn">Add activity here</button>';
+    const info =
+      '<p>Coordinates: ' + this.formatCoordinatesToDispplay(e.latlng) + '</p>';
+    const button =
+      '<button class="btn btn-dark" id="addActivityBtn" style="margin: 5px;">Add activity here</button>';
+    const bikeButton =
+      '<button class="btn btn-outline-dark" id="bikeButton" style="margin: 5px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bicycle" viewBox="0 0 16 16">' +
+      '<path d="M4 4.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1v.5h4.14l.386-1.158A.5.5 0 0 1 11 4h1a.5.5 0 0 1 0 1h-.64l-.311.935.807 1.29a3 3 0 1 1-.848.53l-.508-.812-2.076 3.322A.5.5 0 0 1 8 10.5H5.959a3 3 0 1 1-1.815-3.274L5 5.856V5h-.5a.5.5 0 0 1-.5-.5m1.5 2.443-.508.814c.5.444.85 1.054.967 1.743h1.139zM8 9.057 9.598 6.5H6.402zM4.937 9.5a2 2 0 0 0-.487-.877l-.548.877zM3.603 8.092A2 2 0 1 0 4.937 10.5H3a.5.5 0 0 1-.424-.765zm7.947.53a2 2 0 1 0 .848-.53l1.026 1.643a.5.5 0 1 1-.848.53z"/>' +
+      '</svg></button>';
+    const walkButton =
+      '<button class="btn btn-outline-dark" id="walkButton" style="margin: 5px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-walking" viewBox="0 0 16 16"><path d="M9.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M6.44 3.752A.75.75 0 0 1 7 3.5h1.445c.742 0 1.32.643 1.243 1.38l-.43 4.083a1.8 1.8 0 0 1-.088.395l-.318.906.213.242a.8.8 0 0 1 .114.175l2 4.25a.75.75 0 1 1-1.357.638l-1.956-4.154-1.68-1.921A.75.75 0 0 1 6 8.96l.138-2.613-.435.489-.464 2.786a.75.75 0 1 1-1.48-.246l.5-3a.75.75 0 0 1 .18-.375l2-2.25Z"/><path d="M6.25 11.745v-1.418l1.204 1.375.261.524a.8.8 0 0 1-.12.231l-2.5 3.25a.75.75 0 1 1-1.19-.914zm4.22-4.215-.494-.494.205-1.843.006-.067 1.124 1.124h1.44a.75.75 0 0 1 0 1.5H11a.75.75 0 0 1-.531-.22Z"/></svg></button>';
+    const gymButton =
+      '<button class="btn btn-outline-dark" id="gymButton" style="margin: 5px;">' +
+      '<img src="assets/vectors/dumbbells-svgrepo-com.svg">' +
+      '</button>';
+    const runButton =
+      '<button class="btn btn-outline-dark" id="runButton" style="margin: 5px;"><img src="assets/vectors/running-svgrepo-com.svg"></button>';
+    const tennisButton =
+      '<button class="btn btn-outline-dark" id="tennisButton" style="margin: 5px;"><img src="assets/vectors/tennis-6-svgrepo-com.svg"></button>';
+
     const popup = L.popup()
       .setLatLng(e.latlng)
-      .setContent(info + button)
+      .setContent(
+        info +
+          // button +
+          bikeButton +
+          walkButton +
+          gymButton +
+          runButton +
+          tennisButton,
+      )
       .openOn(this.map);
 
     const addButton = document.getElementById('addActivityBtn');
     if (addButton) {
-      addButton.addEventListener('click', this.saveActivityLocation.bind(this, e.latlng, "activity"));
+      addButton.addEventListener(
+        'click',
+        this.saveActivityLocation.bind(this, e.latlng, 'activity'),
+      );
     }
 
     // // Tworzenie dropdowna z opcjami aktywności fizycznych
@@ -117,31 +149,32 @@ export class MapComponent implements OnInit {
     //   .setLatLng(e.latlng)
     //   .setContent(popupContent)
     //   .openOn(this.map);
-
   }
 
   saveActivityLocation(coordinates: LatLng, activity: string) {
     this.mapService.saveActivity({
-      'coordinates': this.convertLatLngToGeoPoint(coordinates),
-      'date': Timestamp.now(),
-      'name': activity
-    })
+      coordinates: this.convertLatLngToGeoPoint(coordinates),
+      date: Timestamp.now(),
+      name: activity,
+    });
     console.log('Saved location');
   }
 
   addMarker(coordinates: LatLng, text: string) {
-    L.marker(coordinates)
-      .addTo(this.map)
-      .bindPopup(text);
+    L.marker(coordinates).addTo(this.map).bindPopup(text);
   }
 
   addAllMarkers() {
     let data;
-    this.mapService.getActivitiesLocations().then(result => {data = result; result.forEach(
-      value => {
-        this.addMarker(this.convertGeoPointToLatLng(value['coordinates']), value['name']+" on "+this.formatDateFromTimestamp(value['date']))
-      })})
-
+    this.mapService.getActivitiesLocations().then((result) => {
+      data = result;
+      result.forEach((value) => {
+        this.addMarker(
+          this.convertGeoPointToLatLng(value['coordinates']),
+          value['name'] + ' on ' + this.formatDateFromTimestamp(value['date']),
+        );
+      });
+    });
   }
 
   convertGeoPointToLatLng(geoPoint: GeoPoint): LatLng {
@@ -156,17 +189,18 @@ export class MapComponent implements OnInit {
     return new GeoPoint(latitude, longitude);
   }
 
-  formatCoordinatesToDispplay(latlng: LatLng){
+  formatCoordinatesToDispplay(latlng: LatLng) {
     let latitude = latlng.lat;
     let longitude = latlng.lng;
-    let formattedCoordinates = "[" + latitude.toFixed(2) + "° N, " + longitude.toFixed(2) + "° E]";
+    let formattedCoordinates =
+      '[' + latitude.toFixed(2) + '° N, ' + longitude.toFixed(2) + '° E]';
     return formattedCoordinates;
   }
 
-  formatDateFromTimestamp(timestamp: Timestamp){
+  formatDateFromTimestamp(timestamp: Timestamp) {
     const milliseconds = timestamp.toMillis();
     const date = new Date(milliseconds);
-    let formattedDate = this.datePipe.transform(date, 'MMMM d, y \'at\' HH:mm');
+    let formattedDate = this.datePipe.transform(date, "MMMM d, y 'at' HH:mm");
     return formattedDate;
   }
 }
